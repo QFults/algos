77121f84fff2ac5949bc35255fb74950d89f6564
Quinton Fults <coding.banjo@gmail.com>
2020-06-25 11:06:13 -0700

    code

---
+++ index.js
@@ -0,0 +1,25 @@
+// Binary Search
+
+// const nums = [10, 34, 56, 67, 93, 120, 137, 168, 259, 280, 311, 342, 413, 514]
+
+// const binarySearch = (arr, x) => {
+//   let i, y
+//   let min = 0
+//   let max = arr.length - 1
+
+//   while (min <= max) {
+//     i = Math.floor((min + max) / 2)
+//     y = arr[i]
+
+//     if (y < x) {
+//       min = i + 1
+//     } else if (x < y) {
+//       max = i - 1
+//     } else {
+//       return i
+//     }
+//   }
+
+//   return false
+// }
+
